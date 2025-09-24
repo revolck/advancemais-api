6b46f6c7c91ad2a239f3814f6d91105336066107
Filipe Marques <filipe@revolck.com.br>
2025-09-24 12:02:23 -0300

    Removendo migrations desnecessarias

--- prisma/migrations/20250203120000_add_turno_metodo_to_cursos_turmas/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."CursosTurmas"
-  ADD COLUMN     "turno" "public"."CursosTurnos" NOT NULL DEFAULT 'INTEGRAL',
-  ADD COLUMN     "metodo" "public"."CursosMetodos" NOT NULL DEFAULT 'ONLINE';
